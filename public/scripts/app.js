23e0e186c9a4ad82dcfa4149741fe125763fb63e
Al OFlaherty <aloemc2@gmail.com>
2017-10-18 19:16:01 +0000

    second commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
-// Test / driver code (temporary). Eventually will get this from the server.
+var data=[
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+]
+
 var tweetData = {
   "user": {
     "name": "Newton",
@@ -16,59 +62,43 @@ var tweetData = {
   "created_at": 1461116232227
 }
 
-//console.log(tweetData.user.name)
+function renderTweets(data) {
+  for (let key of data){
+    let $tweet=createTweetElement(key)
+    $('#tweets-container').append($tweet);
 
-// function createTweetElement(tweet) {
-//   //var $tweet =
-//    $('article').addClass('tweet').append('header span .nameleft').addClass(nameleft).text(tweetData.user.name);
-//    //$('article').addClass('tweet').append('span .nameright').text(tweetData.user.handle);
-//    //$('article').addClass('tweet').append('span .smilelogo img').text(tweetData.user.name.avatars.regular);
+  }
 
-//   console.log($('<article>'));
-//   //console.log($tweet);
-//   return $tweet;
-// }
-function renderTweets(tweets) {
-  // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
 }
 
+renderTweets(data);
 
-var $tweet = createTweetElement(tweetData);
-console.log($tweet)
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
 
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-function createTweetElement(tweet) {
+function createTweetElement(twt) {
 var $tweet =
 
   `<article>
     <header class="header">
-      <span class="smilelogo"><img src=${tweetData.user.avatars.regular}></span>
+      <span class="smilelogo"><img src=${twt.user.avatars.regular}></span>
 
 
-      <span class="nameleft">${tweetData.user.name}</span>
-      <span class="nameright">${tweetData.user.handle}</span>
+      <span class="nameleft">${twt.user.name}</span>
+      <span class="nameright">${twt.user.handle}</span>
     </header>
       <body>
-        <div class="data">${tweetData.content.text}</div>
+        <div class="data">${twt.content.text}</div>
       </body>
     <footer>
       <div class="footer">
-      <span class="date">${tweetData.created_at}</span>
+      <span class="date">${twt.created_at}</span>
       <span class="flag"><img src="images/flag.png"></span>
       <span class="arrow"><img src="images/arrow.png"></span>
       <span class="heart"><img src="images/heart.png"></span>
     </div >
     </footer>
   </article>`
-//</section>}
 
 return $tweet;
-
 
 };
 
